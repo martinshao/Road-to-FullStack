c6863324ef1572f0c77d98145ac98b36c1933ab9
Myron <shaogucheng@hotmail.com>
2019-09-06 16:25:30 +0800

    提交

---
+++ front-end-interview/code/code3.js
@@ -0,0 +1,5 @@
+setImmediate(() => console.info('setImmediate'));
+process.nextTick(() => console.log(1));
+Promise.resolve().then(() => console.log(2));
+process.nextTick(() => console.log(3));
+Promise.resolve().then(() => console.log(4));
