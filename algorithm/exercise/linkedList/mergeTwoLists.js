3f1cad5860deed1ae1e29c867d91119b290f174f
Martin-Shao <shaogucheng@hotmail.com>
2020-07-21 21:58:09 +0800

    Martin: 算法研究

--- algorithm/exercise/linkedList/mergeTwoLists.js
+++ algorithm/exercise/linkedList/mergeTwoLists.js
@@ -42,9 +42,9 @@ let l2 = ArrayToLinkedList([1, 3, 4]);
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
-    if (l1 == null) {
+    if (l1 === null) {
         return l2;
-    } else if (l2 == null) {
+    } else if (l2 === null) {
         return l1;
     } else if (l1.val < l2.val) {
         l1.next = mergeTwoLists(l1.next, l2);
