6785722fc185f2410c39fefcdbe3e6521861e90a
Martin-Shao <shaogucheng@hotmail.com>
2020-11-12 23:44:34 +0800

    🚧 深拷贝练习，每天一练

--- front-end-interview/about-diy/practice.js
+++ front-end-interview/about-diy/practice.js
@@ -1,34 +1,99 @@
-class EventBus {
+const mapType = '[object Map]';
+const setType = '[object Set]';
+const arrayType = '[object Array]';
+const objectType = '[object object]';
+const argsType = '[object arguments]';
 
-  constructor() {
-    this.subscription = {}
-  }
+const boolType = '[object Boolean]';
+const dateType = '[object Date]';
+const numberType = '[object Number]';
+const stringType = '[object String]';
+const symbolType = '[object Symbol]';
+const errorType = '[object Error]';
+const regexpType = '[object RegExp]';
+const funcType = '[object function]';
 
-  subscription (eventType, callback) {
-    const id = Symbol('id')
-    if(!this.subscription[eventType]) this.subscription[eventType] = {}
-    this.subscription[eventType][id] = callback
-    return {
-      unSubscribe: (function unSubscribe() {
-        delete this.subscription[eventType][id]
-        if(!Object.getOwnPropertySymbols(this.subscription[eventType]).length) {
-          delete this.subscription[eventType]
-        }
-      }).bind(this)
-    }
-  }
+const deepType = [mapType, setType, arrayType, objectType, argsType];
 
-  publish(eventType, arg) {
-    if(!this.subscription[eventType]) return;
+function isObject(obj) {
+  const type = typeof obj;
+  return type !== null && (type === 'object' || type === 'function');
+}
 
-    Object.getOwnPropertySymbols(this.subscription[eventType]).forEach(
-      key => this.subscription[eventType][key](arg)
-    )
+function getInit(target) {
+  const Ctor = target.constructor;
+  return new Ctor();
+}
+
+function cloneSymbol(target) {
+  return Object(Symbol.prototype.valueOf.call(target));
+}
+
+function cloneReg(target) {
+  const reFlags = /\w*$/;
+  const result = new target.constructor(target.source, reFlags.exec(target));
+  result.lastIndex = target.lastIndex;
+  return result;
+}
+
+function cloneFunction(target) {
+  return target;
+}
+
+function cloneOtherType(target) {
+  const Ctor = target.constructor;
+  switch (type) {
+    case boolType:
+    case dateType:
+    case stringType:
+    case numberType:
+    case errorType:
+      return new Ctor(target);
+    case regexpType:
+      return cloneReg(target);
+    case symbolType:
+      return cloneSymbol(target);
+    case funcType:
+      return cloneFunction(target);
+    default:
+      return null;
   }
 }
 
-Function.prototype.bind
+const getType = (target) => Object.prototype.toString.call(target);
 
-const fn = () => {}
+function clone(target, map = new WeakMap()) {
+  if (!isObject(target)) {
+    return target;
+  }
 
-fn.bind(this)
+  const type = getType(target);
+  let cloneTarget;
+  if (deepType.includes(type)) {
+    cloneTarget = getInit(target, type);
+  } else {
+    return cloneOtherType(target);
+  }
+
+  if (map.get(target)) {
+    return target;
+  }
+  map.set(target, cloneTarget);
+
+  if (type === setType) {
+  }
+  if (type === mapType) {
+  }
+
+  const keys =
+    type === arrayType
+      ? undefined
+      : Object.keys(target)
+
+      (keys || target).forEach((value, key) => {
+        if (keys) {
+          key = value;
+        }
+        cloneTarget[key] = clone(target[key], map);
+      });
+}
