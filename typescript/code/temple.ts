bbc85043426e6da630f758c84cb14dfe05c599ff
Martin-Shao <shaogucheng@hotmail.com>
2022-08-12 14:48:18 +0800

    📝 about typescipt

--- typescript/code/temple.ts
+++ typescript/code/temple.ts
@@ -1,18 +1,22 @@
-interface KlineBlocBase {
-    name: string
+interface Person {
+    name: string;
+    age: number;
+    sex: string;
 }
 
-class KlineBlocProvider<T extends KlineBlocBase> extends StatefulWidget {
-    KlineBlocProvider({ Key key, @required this.child, @required this.bloc})
-        : super(key: key);
-final Widget child;
-final T bloc;
-@override
-_KlineBlocProviderState < T > createState() => _KlineBlocProviderState<T>();
-    static T of<T extends KlineBlocBase>(BuildContext context) {
-    final type = _typeOf<KlineBlocProvider<T>>();
-    KlineBlocProvider < T > provider = context.ancestorWidgetOfExactType(type);
-    return provider.bloc;
+interface Height {
+    height: number;
+    face: string;
+    name: string;
 }
-    static Type _typeOf<T>() => T;
-  }
+
+type And<T, U> = T & U;
+
+type Or<T, U> = T | U;
+
+type PersonOfKeys = keyof Person;
+type HeightOfKeys = keyof Height;
+
+const a: PersonOfKeys = {
+    name: '123'
+}
